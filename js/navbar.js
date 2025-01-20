d0a7a065151b6a893e2037551df0a67a333a79a7
Claudia <46584089+Claudia-J@users.noreply.github.com>
2025-01-20 15:29:47 +0100

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -6,9 +6,15 @@ const url = window.location.pathname;
 const basePath = url.includes('/views/') ? '../' : './';
 
 fetch(`${basePath}assets/data/datos.json`)
-     .then(response => response.json())
+     .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error al cargar JSON: ${response.statusText}`);
+        }
+        return response.json();
+    })
      .then(data => {
-        data.navItems.forEach(link => {
+          console.log(data);
+          data.navItems.forEach(link => {
 
            // Ajustar la URL eliminando posibles barras redundantes
            const adjustedUrl = `${basePath}${link.url}`.replace(/\/{2,}/g, '/');
